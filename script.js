2d2495f41dc96c4205b2ea53e2467c2c1af82c51
Gehrig Wilcox <lougehrig10@gmail.com>
2021-06-15 19:41:10 -0500

    Added page title and fixed code

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 window.addEventListener('DOMContentLoaded',()=>{
-  document.getElementById('command').innerHTML = "/configSP " + document.location.hash.match("(?:access_token=)[^&]*")[0];
+  document.getElementById('command').innerHTML = "/configSP " + document.location.hash.match("(?:access_token=)[^&]*")[0].replace("access_token=","");
 });
