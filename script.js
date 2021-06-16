019d6d274d03b7d87fd113d83e7cc3465b0f795a
Gehrig Wilcox <lougehrig10@gmail.com>
2021-06-15 19:38:43 -0500

    Fixed code

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 window.addEventListener('DOMContentLoaded',()=>{
-  document.getElementById('command').innerHTML = "/configSP " + document.location.hash.access_token;
+  document.getElementById('command').innerHTML = "/configSP " + document.location.hash.match("(?:access_token=)[^&]*")[0];
 });
